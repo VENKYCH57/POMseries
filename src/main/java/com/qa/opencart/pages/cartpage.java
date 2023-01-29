f3b27265279aaccce8fc61e2b7721b3560e9464a
venkatesh chilukuri <venkych57@gmail.com>
2023-01-29 12:33:06 +0530

    updated cart feature

--- src/main/java/com/qa/opencart/pages/cartpage.java
+++ src/main/java/com/qa/opencart/pages/cartpage.java
@@ -2,4 +2,12 @@ package com.qa.opencart.pages;
 
 public class cartpage {
 
+	public cartpage() {
+		System.out.println("this is my cart page");
+	}
+
+	public void getcartpage() {
+		System.out.println("this is my cartpage method");
+	}
+
 }
